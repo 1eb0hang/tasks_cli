8f33c76621cd5d5deae60447138ad8013fa89c80
Lebohang Mokhwibitjane <lebohangmokhwibitjane@gmail.com>
2025-04-12 23:32:07 +0200

    added command line argument support

--- index.js
+++ index.js
@@ -1,10 +1,41 @@
-import http from "http"
+import process from "node:process";
+import * as fs from "node:fs"
 
-const  server = http.createServer((req, res)=>{
-    res.write("<h1>hello owlrd</h1>");
-    res.end();
-});
+const commands = {"add" : addTask,    // add new task 
+		  "update" : updateTask, // update task 
+		  "delete" : deleteTask, // delete task
+		  "mark" : markTask,   // mark task (todo, in-progress, done)
+		  "list" : listTasks}   // list tasks (all, done, todo, im-progress)
 
-server.listen(8080, ()=>{
-   console.log("Server running on port 8080"); 
-});
+let args = process.argv.splice(2);
+
+function addTask(command){
+    console.log("added task: " + command[0])
+}
+
+function updateTask(command){
+    console.log("updated task: " + command[0])
+}
+
+function deleteTask(command){
+    console.log("deleted task: " + command[0])
+}
+
+function markTask(command){
+    console.log("marked task: " + command[0])
+}
+
+function listTasks(command){
+    console.log("listing tasks: " + command[0])
+}
+
+function handleCommand(args){
+    let command = args[0].trim().toLowerCase();
+    if(Object.keys(commands).includes(command)){
+	commands[args[0]](["hello world"]);
+    }else{
+	console.log("Command not found: " + args[0]);
+    }
+}
+
+handleCommand(args)
